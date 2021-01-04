efe992f731c12b0f110a2521d5dfcce3e75cbbde
Angela Mitchell <anglarenmit@gmail.com>
2021-01-04 11:14:26 -0600

    Created a custom async validator to check for forbidden emails

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -27,7 +28,11 @@ export class AppComponent implements OnInit {
           Validators.required,
           this.forbiddenNames.bind(this),
         ]),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(
+          null,
+          [Validators.required, Validators.email],
+          this.forbiddenEmails
+        ),
       }),
       gender: new FormControl('female'),
       hobbies: new FormArray([]),
@@ -47,6 +52,19 @@ export class AppComponent implements OnInit {
     if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
       return { nameIsForbidden: true };
     }
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ emailIsForbidden: true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 
   onSubmit() {
