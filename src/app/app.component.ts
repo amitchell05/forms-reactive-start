c3a9e530f63a0cf91d6e663170f72518f305c1f0
Angela Mitchell <anglarenmit@gmail.com>
2021-01-04 11:30:54 -0600

    Observed form changes with valueChanges and statusChanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,6 +37,8 @@ export class AppComponent implements OnInit {
       gender: new FormControl('female'),
       hobbies: new FormArray([]),
     });
+    // this.signupForm.valueChanges.subscribe((value) => console.log(value));
+    this.signupForm.statusChanges.subscribe((status) => console.log(status));
   }
 
   onAddHobby() {
