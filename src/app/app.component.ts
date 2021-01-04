81dd71b794e38a171700a4450fc7368450b2a4f2
Angela Mitchell <anglarenmit@gmail.com>
2021-01-04 11:44:56 -0600

    Set and patched values to prepopulate form fields; called reset method on form to clear values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,6 +39,19 @@ export class AppComponent implements OnInit {
     });
     // this.signupForm.valueChanges.subscribe((value) => console.log(value));
     this.signupForm.statusChanges.subscribe((status) => console.log(status));
+    this.signupForm.setValue({
+      userData: {
+        username: 'Max',
+        email: 'max@test.com',
+      },
+      gender: 'male',
+      hobbies: [],
+    });
+    this.signupForm.patchValue({
+      userData: {
+        username: 'Anna',
+      },
+    });
   }
 
   onAddHobby() {
@@ -71,5 +84,6 @@ export class AppComponent implements OnInit {
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 }
